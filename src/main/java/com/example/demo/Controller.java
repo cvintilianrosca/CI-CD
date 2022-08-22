cecfb6aaa8e32f9df67899375cfbd0dcc17dca86
rosca <cvintilian.rosca@hpe.com>
2022-08-22 15:46:01 +0300

    sss

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -12,6 +12,6 @@ public class Controller {
 
     @GetMapping
     public ResponseEntity<String> process(){
-        return ResponseEntity.ok("Nanana");
+        return ResponseEntity.ok("Nanana1");
     }
 }
